8cf7e865d5d716b9d1e3eb10506b74922f64a1f5
Tomas Hykel <mail@thykel.cz>
2026-06-15 17:19:08 +0200

    Use with_item_group in enumeration component

--- app/components/admin/enumerations/item_component.rb
+++ app/components/admin/enumerations/item_component.rb
@@ -55,20 +55,18 @@ module Admin
       end
 
       def build_enumeration_menu(menu)
-        edit_enumeration(menu)
-        menu.with_divider
-        if !first_item? || !last_item?
-          if !first_item?
+        with_item_group(menu) { edit_enumeration(menu) }
+        with_item_group(menu) do
+          unless first_item?
             move_to_top_enumeration(menu)
             move_up_enumeration(menu)
           end
-          if !last_item?
+          unless last_item?
             move_down_enumeration(menu)
             move_to_bottom_enumeration(menu)
           end
-          menu.with_divider
         end
-        deletion_enumeration(menu)
+        with_item_group(menu) { deletion_enumeration(menu) }
       end
 
       def edit_enumeration(menu)
